18fa719a7966d621092a435048f74b3985874373
angrygoose0 <131853530+angrygoose0@users.noreply.github.com>
2025-08-26 10:39:42 +1200

    supabase integration worked!

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = "https://lxvwqaywsjcpaudyzysj.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4dndxYXl3c2pjcGF1ZHl6eXNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwOTc1ODAsImV4cCI6MjA3MTY3MzU4MH0.x9oq05xbKKxhFDXRAMB5ob6_SrKMQTpGpr0QrGumh3E";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
 
 window.signup = function signup() {
   return {
@@ -18,7 +16,7 @@ window.signup = function signup() {
       this.loading = true;
       this.message = '';
       try {
-        const { error } = await supabase.from('signups').insert({ email: this.formEmail });
+        const { error } = await supabase.from('pre-sale').insert({ email: this.formEmail });
         if (error) {
           this.message = 'Something went wrong. Please try again.';
           console.error(error);
